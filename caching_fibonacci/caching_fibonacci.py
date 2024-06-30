5aeab077fb628933eca9b8614411eb40d0b8df56
Natalya Karpova <knataly0513@gmail.com>
2024-06-30 11:36:01 +0300

    Add file of task-1

---
+++ caching_fibonacci/caching_fibonacci.py
@@ -0,0 +1,37 @@
+""" Calculates the Fibonacci numbers creating and using a cache to store and reuse already calculated values of Fibonacci numbers """
+
+def caching_fibonacci() -> callable:
+    """Calculates the Fibonacci numbers creating and using a cache to store and reuse already calculated values of Fibonacci numbers
+    
+    Raises:
+        TypeError: raises an error when parameters in the function are not an integer n < 0
+
+    Returns:
+        callable: returns the inner function fibonacci(n) that calculates the Fibonacci number using a cache from an external function
+    """
+
+    cache = {}
+
+    def fibonacci(number: int) -> int:
+
+        try:
+            if not isinstance(number, int) or number < 0:
+                raise TypeError
+            if number <= 1:
+                return number
+            if number in cache:
+                return cache[number]
+            cache[number] = fibonacci(number - 1) + fibonacci(number - 2)
+            return cache[number]
+
+        except TypeError:
+            return "Помилка даних: значення параметру повинно бути цілим числом, більшим за нуль"
+    return fibonacci
+
+
+# Отримуємо функцію fibonacci
+fib = caching_fibonacci()
+
+# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+print(fib(10))  # Виведе 55
+print(fib(15))  # Виведе 610
