222345ed9d6c20c17a3d489f6bd5dcad24927cdc
Natalya Karpova <knataly0513@gmail.com>
2024-07-01 10:21:53 +0300

    add hw-4 files

---
+++ task-4_bot_cli/handlers/__init__.py
@@ -0,0 +1,5 @@
+from handlers.parse_input.parse_input import parse_input
+from handlers.contacts_handlers.contacts_handlers import add_contact, change_contact, show_all, show_phone, delete_contact
+# from handlers.input_error.input_error import input_error
+
+__all__ = [parse_input,  add_contact, change_contact, show_all, show_phone, delete_contact]
